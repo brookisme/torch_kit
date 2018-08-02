332628ee39c023b2ebc531559663f94b404c9225
Brookie Guzder-Williams <bguzder-williams@wri.org>
2018-08-02 10:11:52 -0700

    functional: move methods from loss

--- pytorch_nns/loss.py
+++ pytorch_nns/loss.py
@@ -1,103 +1,41 @@
 import torch.nn as nn
-from torch.autograd import Variable, Function
-#
-# CONFIG
-#
-EPS=1e-11
+import pytorch_nns.functional as F
 
-
-#
-# HELPERS
-#
-def _to_tensor(v):
-    if isinstance(v,list):
-        v=torch.tensor(v)
-    elif isinstance(np.ndarray):
-        v=torch.from_numpy(v)
-    return v
-
-
-
-
-#**********************************************************************
-#
-# WEIGHTED CATEGORICAL CROSS ENTROPY
-#
-#**********************************************************************
-def weighted_categorical_crossentropy(weights):
-    """ weighted_categorical_crossentropy
-
-        Args:
-            * weights<tensor|np.array|list>: crossentropy weights
-        Returns:
-            * weighted categorical crossentropy function
-    """
-    weights=_to_tensor(weights)
-    def loss(inpt,targ):
-        targ=torch.tensor(targ)
-        inpt=torch.tensor(inpt)
-        inpt/=(inpt.sum(1,True)+EPS)
-        inpt=torch.clamp(inpt, EPS, 1. - EPS)
-        losses=((targ * torch.log(inpt)))
-        weighted_losses_transpose=(weights.float()*losses.transpose(1,-1).float())
-        return -weighted_losses_transpose.mean()
-    return loss
 
 
 class WeightedCategoricalCrossentropy(nn.Module):
     """ weighted_categorical_crossentropy
+        
+        mean reduction of weighted categorical crossentropy
 
         Args:
-            * weights<ktensor|nparray|list>: crossentropy weights
-        Returns:
-            * weighted categorical crossentropy function
+            * weights<tensor|nparray|list>: category weights
     """
     def __init__(self, weights):
         super(WeightedCategoricalCrossentropy, self).__init__()
-        self.loss=weighted_categorical_crossentropy(weights)
+        self.weights=F.to_tensor(weights)
 
         
     def forward(self, inpt, targ):
-        return self.loss(inpt,targ)
-
-
-
-
-#**********************************************************************
-#
-# WEIGHTED DICE
-#
-#**********************************************************************
-def dice(weights=None):
-    if weights:
-        weights=_to_tensor(weights).float()
-    def loss(inpt, targ):
-        targ=targ.float()
-        inpt=(inpt>EPS).float()
-        inpt=Variable(inpt, requires_grad=True)
-        if weights:
-            inpt=(weights*inpt.transpose(1,-1)).transpose(1,-1)
-        dice_coefs=2.0*(inpt*targ).sum(1,False)/(inpt+targ).sum(1,False)
-        return dice_coefs.mean()
-    return loss
+        return F.weighted_categorical_crossentropy(inpt,targ,self.weights)
 
 
 
 class Dice(nn.Module):
-    """ DiceLoss (optionally weighted)
+    """ (optionally weighted) dice loss
+        
+        mean reduction of (weighted) dice
 
         Args:
-            * weights<ktensor|nparray|list|None>: crossentropy weights
-        Returns:
-            * weighted categorical crossentropy function
+            * weights<tensor|nparray|list|None>: optional category weights
     """
-    def __init__(self, weights=None):
+    def __init__(self, weights):
         super(Dice, self).__init__()
-        self.loss=weighted_categorical_crossentropy(weights)
+        self.weights=F.to_tensor(weights)
 
         
     def forward(self, inpt, targ):
-        return self.loss(inpt,targ)
+        return F.dice(inpt,targ,self.weights)
 
 
 
