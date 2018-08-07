models/unet: add softmax on output.  fix order of SE to match with kgraph unet

pytorch_nns/functional.py
@@ -25,7 +25,7 @@ def weighted_categorical_crossentropy(inpt,targ,weights):
     inpt=torch.clamp(inpt, EPS, 1. - EPS)
     losses=((targ * torch.log(inpt))).float()
     weighted_losses_transpose=weights*losses.transpose(1,-1)
-    return -weighted_losses_transpose.mean()
+    return -weighted_losses_transpose.mean()*targ.size(1)
 
 
 
@@ -47,4 +47,35 @@ def dice(inpt,targ,weights=None):
     if weights is not None:
         inpt=(weights*inpt.transpose(1,-1)).transpose(1,-1)
     dice_coefs=2.0*(inpt*targ).sum(1,False)/(inpt+targ+EPS).sum(1,False)
-    return dice_coefs.mean()
+    return -dice_coefs.mean()
+
+
+def soft_dice_loss(y_true, y_pred, epsilon=1e-6): 
+    ''' TODO: COMPARE MY DICE TO THIS
+
+    https://www.jeremyjordan.me/semantic-segmentation/#loss
+    
+    ...
+
+    Soft dice loss calculation for arbitrary batch size, number of classes, and number of spatial dimensions.
+    Assumes the `channels_last` format.
+  
+    # Arguments
+        y_true: b x X x Y( x Z...) x c One hot encoding of ground truth
+        y_pred: b x X x Y( x Z...) x c Network output, must sum to 1 over c channel (such as after softmax) 
+        epsilon: Used for numerical stability to avoid divide by zero errors
+    
+    # References
+        V-Net: Fully Convolutional Neural Networks for Volumetric Medical Image Segmentation 
+        https://arxiv.org/abs/1606.04797
+        More details on Dice loss formulation 
+        https://mediatum.ub.tum.de/doc/1395260/1395260.pdf (page 72)
+        
+        Adapted from https://github.com/Lasagne/Recipes/issues/99#issuecomment-347775022
+    '''
+    
+    # skip the batch and class axis for calculating Dice score
+    axes = tuple(range(1, len(y_pred.shape)-1)) 
+    numerator = 2. * np.sum(y_pred * y_true, axes)
+    denominator = np.sum(np.square(y_pred) + np.square(y_true), axes)
+    return 1 - np.mean(numerator / (denominator + epsilon)) # average over classes and batch

pytorch_nns/models/shared/blocks.py
@@ -49,10 +49,11 @@ class Conv(nn.Module):
         kernel_size (int <3>): Kernal Size
         stride (int <1>): Stride
         padding (int <0>): Padding
+        in_block_relu (bool <True>): apply relu after conv layer in block
         bn (bool <True>): Add batch norm layer
         se (bool <True>): Add Squeeze and Excitation Block
-        act (str <'relu'>): Method name of activation function
-        act_kwargs (dict <{}>): Kwargs for activation function
+        act (str <'relu'>): Method name of activation function for output of block
+        act_kwargs (dict <{}>): Kwargs for activation function for output of block
         
     Properties:
         out_ch <int>: Number of channels of the output
@@ -67,9 +68,10 @@ class Conv(nn.Module):
             kernel_size=3, 
             stride=1, 
             padding=0, 
-            bn=True,
-            se=True,
-            act='relu',
+            in_block_relu=True,
+            bn=False,
+            se=False,
+            act=None,
             act_kwargs={}):
         super(Conv, self).__init__()
         self.out_ch=out_ch or 2*in_ch
@@ -79,7 +81,8 @@ class Conv(nn.Module):
             in_ch,
             kernel_size,
             stride,
-            padding)
+            padding,
+            in_block_relu)
         self.out_size=in_size-depth*2*((kernel_size-1)/2-padding)
 
         
@@ -113,7 +116,8 @@ class Conv(nn.Module):
             in_ch,
             kernel_size,
             stride,
-            padding):
+            padding,
+            in_block_relu):
         layers=[]
         for index in range(depth):
             if index!=0:
@@ -125,6 +129,8 @@ class Conv(nn.Module):
                     kernel_size=kernel_size,
                     stride=stride,
                     padding=padding))
+            if in_block_relu:
+                layers.append(nn.ReLU())
         self.conv_layers=nn.Sequential(*layers)
 
         

pytorch_nns/models/unet/blocks.py
@@ -19,7 +19,7 @@ class Down(nn.Module):
         depth (int <2>): The number of convolutional layers 
         padding (int <0>): Padding. Use 0 or 1 since the kernal size is fixed at 3x3
         bn (bool <True>): Add batch norm layer after Conv Block
-        se (bool <True>): Add Squeeze and Excitation Block after Conv Block
+        se (bool <True>): Add Squeeze and Excitation Block after Max Pool
         act (str <'relu'>): Method name of activation function after Conv Block
         act_kwargs (dict <{}>): Kwargs for activation function after Conv Block
         
@@ -34,14 +34,18 @@ class Down(nn.Module):
             out_ch=None,
             depth=2,
             padding=0,
-            bn=True,
+            bn=False,
             se=True,
-            act='relu',
+            act=None,
             act_kwargs={}):
         super(Down, self).__init__()
         self.out_size=(in_size//2)-depth*(1-padding)*2
         self.out_ch=out_ch or in_ch*2
         self.down=nn.MaxPool2d(kernel_size=2)
+        if se:
+            self.se=blocks.SqueezeExcitation(in_ch)
+        else:
+            self.se=False
         self.conv_block=blocks.Conv(
             in_ch=in_ch,
             out_ch=self.out_ch,
@@ -49,13 +53,14 @@ class Down(nn.Module):
             depth=depth,
             padding=padding,
             bn=bn,
-            se=se,
             act=act,
             act_kwargs=act_kwargs)
 
         
     def forward(self, x):
         x=self.down(x)
+        if self.se:
+            x=self.se(x)
         return self.conv_block(x)
 
 
@@ -110,9 +115,9 @@ class Up(nn.Module):
             bilinear=False,
             crop=None,
             padding=0,
-            bn=True,
+            bn=False,
             se=True,
-            act='relu',
+            act=None,
             act_kwargs={}):
         super(Up, self).__init__()
         self.crop=crop
@@ -122,7 +127,7 @@ class Up(nn.Module):
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.up = nn.ConvTranspose2d(in_ch, in_ch//2, 2, stride=2)
+            self.up = nn.ConvTranspose2d(in_ch, self.out_ch, 2, stride=2)
         self.conv_block=blocks.Conv(
             in_ch,
             self.out_size,
@@ -130,9 +135,12 @@ class Up(nn.Module):
             depth=depth,
             padding=padding,
             bn=bn,
-            se=se,
             act=act,
             act_kwargs=act_kwargs)
+        if se:
+            self.se=blocks.SqueezeExcitation(self.out_ch)
+        else:
+            self.se=False
         
         
     def forward(self, x, skip):
@@ -140,6 +148,8 @@ class Up(nn.Module):
         skip = self._crop(skip,x)
         x = torch.cat([skip, x], dim=1)
         x = self.conv_block(x)
+        if self.se:
+            x=self.se(x)
         return x
 
     

pytorch_nns/models/unet/model.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
-from pytorch_nns.models.shared.blocks import Conv
+import torch.nn.functional as F
+from pytorch_nns.models.shared.blocks import Conv as ConvBlock
 import pytorch_nns.models.unet.blocks as blocks
 
 
@@ -42,39 +43,28 @@ class UNet(nn.Module):
             out_ch=2,
             init_ch=64,
             padding=0,
-            bn=True,
-            se=True,
-            act='relu',
-            act_kwargs={}):
+            bn=False,
+            se=True):
         super(UNet, self).__init__()
         self.network_depth=network_depth
         self.conv_depth=conv_depth
         self.out_ch=out_ch
         self.padding=padding
-        self.input_conv=Conv(
+        self.input_conv=ConvBlock(
             in_ch=in_ch,
             in_size=in_size,
             out_ch=init_ch,
             depth=self.conv_depth,
             padding=padding,
-            bn=bn,
-            se=se,
-            act=act,
-            act_kwargs=act_kwargs)
+            bn=False,
+            se=False)
         down_layers=self._down_layers(
             self.input_conv.out_ch,
             self.input_conv.out_size,
             bn=bn,
-            se=se,
-            act=act,
-            act_kwargs=act_kwargs)
+            se=se)
         self.down_blocks=nn.ModuleList(down_layers)
-        up_layers=self._up_layers(
-            down_layers,
-            bn=bn,
-            se=se,
-            act=act,
-            act_kwargs=act_kwargs)
+        up_layers=self._up_layers(down_layers,bn=bn,se=se)
         self.up_blocks=nn.ModuleList(up_layers)
         self.out_size=self.up_blocks[-1].out_size
         self.output_conv=self._output_layer(out_ch)
@@ -91,13 +81,13 @@ class UNet(nn.Module):
         for skip,block in zip(skips,self.up_blocks):
             x=block(x,skip)
         x=self.output_conv(x)
-        return x
+        return F.softmax(x)
     
     
     #
     # Internal Methods
     #
-    def _down_layers(self,in_ch,in_size,bn,se,act,act_kwargs):
+    def _down_layers(self,in_ch,in_size,bn,se):
         layers=[]
         for index in range(1,self.network_depth+1):
             layer=blocks.Down(
@@ -106,23 +96,21 @@ class UNet(nn.Module):
                 depth=self.conv_depth,
                 padding=self.padding,
                 bn=bn,
-                se=se,
-                act=act,
-                act_kwargs=act_kwargs)
+                se=se)
             in_ch=layer.out_ch
             in_size=layer.out_size
             layers.append(layer)
         return layers
 
         
-    def _up_layers(self,down_layers,bn,se,act,act_kwargs):
+    def _up_layers(self,down_layers,bn,se):
         down_layers=down_layers[::-1]
         down_layers.append(self.input_conv)
         first=down_layers.pop(0)
         in_ch=first.out_ch
         in_size=first.out_size
         layers=[]
-        for down_layer in down_layers:
+        for down_layer in down_layers:       
             crop=blocks.Up.cropping(down_layer.out_size,2*in_size)
             layer=blocks.Up(
                 in_ch,
@@ -131,9 +119,7 @@ class UNet(nn.Module):
                 crop=crop,
                 padding=self.padding,
                 bn=bn,
-                se=se,
-                act=act,
-                act_kwargs=act_kwargs)
+                se=se)
             in_ch=layer.out_ch
             in_size=layer.out_size
             layers.append(layer)

pytorch_nns/train.py
@@ -1,3 +1,4 @@
+import time
 import torch.cuda
 import torch.optim as optim
 import torch.nn as nn
@@ -48,6 +49,7 @@ def fit(
         This is an early pass based on 
         https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html?highlight=dataloader#train-the-network
     """
+    model.train()
     steps=len(dataloader)
     print_freq=max(steps*noise_reducer//MAX_DOTS,1)
     h.print_line()
@@ -56,16 +58,17 @@ def fit(
         epoch_loss=0.0
         print_epoch=(not (noise_reducer and (epoch%noise_reducer)))
         for index, batch in enumerate(dataloader):
+            optimizer.zero_grad()
             inputs=batch[inputs_key].float()
             targets=batch[targets_key].float()
             if device:
                 inputs=inputs.to(device)
                 targets=targets.to(device) 
-            optimizer.zero_grad()
             outputs=model(inputs)
             if output_processor:
                 outputs=output_processor(outputs,**output_processor_kwargs)
             loss=criterion(outputs, targets)
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             # print statistics
@@ -89,3 +92,168 @@ def fit(
         print_batch_end(epoch,index,steps,loss_str)
     h.print_line()
     print('COMPLETE: loss={:.5f}'.format(epoch_loss))
+    model.train(mode=False)
+
+"""
+https://github.com/pytorch/examples/blob/master/imagenet/main.py
+"""
+
+
+def run(model, train_loader, criterion, optimizer,device=None, nb_epochs=2, noise_reducer=1):
+    best_prec1=0
+    for epoch in range(0,nb_epochs):
+        # train for one epoch
+        train(train_loader, model, criterion, optimizer, epoch, device, noise_reducer)
+
+        # evaluate on validation set
+        prec1 = validate(train_loader, model, criterion, device, noise_reducer)
+
+        # remember best prec@1 and save checkpoint
+        is_best = prec1 > best_prec1
+        best_prec1 = max(prec1, best_prec1)
+        print('-'*70)
+        print("best[{}]: ".format(epoch),is_best,best_prec1)
+        print('-'*70)
+
+
+def train(train_loader, model, criterion, optimizer, epoch, device, noise_reducer):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, b in enumerate(train_loader):
+        input=b['input'].float()
+        target=b['target'].float()
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        if device is not None:
+            input=input.to(device)
+            target=target.to(device)
+
+        # compute output
+        output = model(input)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec1.item(), input.size(0))
+        # top5.update(prec5[0], input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        report_tmpl="Epoch: [{0}][{1}/{2}],  Time {batch_time.val:.3f} ({batch_time.avg:.3f}),  Data {data_time.val:.3f} ({data_time.avg:.3f}),  Loss {loss.val:.4f} ({loss.avg:.4f}),  Prec@1 {top1.val:.3f} ({top1.avg:.3f})"
+        if i % noise_reducer == 0:
+            report=report_tmpl.format(
+                   epoch, i, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1)
+            print(report,end="\r",flush=True)
+    print(report,flush=True)
+
+
+def validate(val_loader, model, criterion, device, noise_reducer):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, b in enumerate(val_loader):
+            input=b['input'].float()
+            target=b['target'].float()
+            if device is not None:
+                input=input.to(device)
+                target=target.to(device)
+
+            # compute output
+            output = model(input)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            prec1, prec5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), input.size(0))
+            top1.update(prec1.item(), input.size(0))
+            # top5.update(prec5[0], input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+            report_tmpl="Test: [{0}/{1}],  Time {batch_time.val:.3f} ({batch_time.avg:.3f}),  Loss {loss.val:.4f} ({loss.avg:.4f}),  Prec@1 {top1.val:.3f} ({top1.avg:.3f})"
+            if i % noise_reducer == 0:
+                report=report_tmpl.format(
+                       i, len(val_loader), batch_time=batch_time, loss=losses,
+                       top1=top1)
+                print(report,end="\r",flush=True)
+        print(report,flush=True)
+        print('='*70)
+    return top1.avg
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    with torch.no_grad():
+        o=torch.argmax(output,dim=1)
+        t=torch.argmax(target,dim=1)
+        batch_size = target.size(0)
+        size=target.size(-1)
+        eqval=o.eq(t)
+        acc=eqval.sum()/(size**2)
+        return acc,acc*10
+        # _, pred = output.topk(maxk, 1, True, True)
+        # pred = pred.t()
+        # correct = pred.eq(target.view(1, -1).expand_as(pred))
+        # res = []
+        # for k in topk:
+        #     correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        #     res.append(correct_k.mul_(100.0 / batch_size))
+        # return res
+