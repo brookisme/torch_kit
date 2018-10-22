9e8bb53f1fb5292d7e45df41ba556eb147a180ce
Brookie Guzder-Williams <bguzder-williams@wri.org>
2018-10-22 14:40:29 -0700

    metrics refactor (untested)

--- pytorch_nns/metrics.py
+++ pytorch_nns/metrics.py
@@ -4,65 +4,74 @@ import pytorch_nns.helpers as h
 from sklearn.metrics import confusion_matrix
 
 
-"""
-
-TODO:
-
-CLEAN UP LOGIC - ALREADY ARGMAXED OR NOT
-
-"""
-def _argmax(preds,targs):
-    if torch.is_tensor(preds):
-        preds=torch.argmax(preds,dim=1)
-        targs=torch.argmax(targs,dim=1)
-    else:
-        preds=np.argmax(preds,axis=1)
-        targs=np.argmax(targs,axis=1)
-    return preds,targs
+def target_prediction_argmax(targ,pred,axis=0):
+    return h.argmax(pred,axis=axis),h.argmax(targ,axis=axis)
 
 
-def confusion(pred,targ,categories):
-    if not isinstance(categories,int):
-        categories=len(categories)
-    # TODO: PURE PyTorch Version
-    pred=h.to_numpy(pred)
-    targ=h.to_numpy(targ)
-    cmatrix=confusion_matrix(
+def accuracy(pred,targ,argmax=True,axis=0):
+    if argmax:
+        targ,pred=target_prediction_argmax(targ,pred,axis=axis)
+    return (pred==targ).float().mean()
+
+
+def confusion(targ,pred,labels=None,nb_categories=None,argmax=False,axis=0):
+    if argmax:
+        if not nb_categories:
+            nb_categories=targ.shape[axis]
+        targ,pred=target_prediction_argmax(targ,pred,axis=axis)
+    if not labels:
+        labels=range(nb_categories)
+    return confusion_matrix(
         targ.reshape(-1),
         pred.reshape(-1),
-        labels=range(categories))
-    return cmatrix
+        labels=labels)
 
 
-def precision(categories,category,cmatrix=None,pred=None,targ=None):
-    if cmatrix is None: 
-        cmatrix=confusion(pred,targ,categories)
-    index=h.get_index(category,categories)
-    if (cmatrix[index].sum()==0):
-        return 1
+def precision(category_index,cmatrix=None,targ=None,pred=None,argmax=False,axis=0):
+    if not cmatrix:
+        cmatrix=confusion(targ,pred,argmax=argmax,axis=axis)
+    tp_plus_fp=cmatrix[:,category_index].sum()
+    if tp_plus_fp==0:
+        return 1.0 
+    else: 
+        return cmatrix[category_index,category_index]/tp_plus_fp
+
+
+def recall(category_index,cmatrix=None,targ=None,pred=None,argmax=False,axis=0):
+    if not cmatrix:
+        cmatrix=confusion(targ,pred,argmax=argmax,axis=axis)
+    tp_plus_fn=cmatrix[category_index].sum()
+    if tp_plus_fn==0:
+        return 1.0
     else:
-        tp_plus_fp=cmatrix[:,index].sum()
-    if (tp_plus_fp==0):
-        return 0
-    else: 
-        return cmatrix[index,index]/tp_plus_fp
+        return cmatrix[category_index,category_index]/tp_plus_fn
 
 
-def recall(categories,category,cmatrix=None,pred=None,targ=None):
-    if cmatrix is None: 
-        cmatrix=confusion(pred,targ)    
-    index=h.get_index(category,categories)
-    if (cmatrix[c].sum()==0):
-        return 1
-    else: 
-        return cmatrix[c,c]/cmatrix[c].sum()
- 
+def fbeta(
+        precision=None,
+        recall=None,
+        category_index=None,
+        cmatrix=None,
+        targ=None,
+        pred=None,
+        beta=DEFAULT_BETA,
+        argmax=False,
+        axis=0
+        return_precision_recall=False):
+    if not precision:
+        cmatrix=confusion(targ,pred,argmax=argmax,axis=axis)
+        precision=precision(category_index,cmatrix=cmatrix)
+        recall=recall(category_index,cmatrix=cmatrix)
+    beta_sq=(beta**2)
+    if (precision==0) and (recall==0):
+        return 0.0
+    else:
+        fbeta=(1+beta_sq)*(precision*recall)/(beta_sq*precision + recall)
+        if return_precision_recall:
+            return fbeta, precision, recall
+        else:
+            return fbeta
 
-def accuracy(pred,targ):
-    pred,targ=_argmax(pred,targ)
-    return (pred==targ).float().mean()
-    # pred=h.to_numpy(pred)
-    # targ=h.to_numpy(targ)
-    # return (pred==targ).astype('float32').mean()
 
 
+         
