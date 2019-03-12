e45fe5bd6f5e3fc9057007888a6a65af5f698a7b
Brookie Guzder-Williams <bguzder-williams@wri.org>
2019-03-12 00:34:15 -0700

    trainer - val_loss/acc, history - dict

--- pytorch_nns/callbacks/__init__.py
+++ pytorch_nns/callbacks/__init__.py
@@ -14,17 +14,21 @@ TARG_KEY='target'
 
 BASE_STATE_ATTRIBUTES=[
     'nb_epochs',
+    'mode',
     'epoch',
-    'batch',
-    'batch_loss',
-    'batch_acc' ]
+    'batch' ]
 
 COMPUTED_STATE_ATTRIBUTES=[
+    'batch_loss',
+    'batch_acc', 
     'total_loss',
     'loss',
     'acc',
-    'best_loss',
-    'best_epoch']
+    'val_batch_loss',
+    'val_batch_acc', 
+    'val_total_loss',
+    'val_loss',
+    'val_acc' ]
 
 STATE_ATTRIBUTES=BASE_STATE_ATTRIBUTES+COMPUTED_STATE_ATTRIBUTES
 CALLBACK_ERROR='Trainer: callbacks already set. use force=True to override'
@@ -87,14 +91,17 @@ class Trainer(object):
             accuracy_method=None,
             round_prediction=False,
             initial_loss=9e12):
-        self._reset_state(nb_epochs=nb_epochs,best_loss=initial_loss)
+        self._reset_state(nb_epochs=nb_epochs)
+        self.best_loss=initial_loss
+        self.best_epoch=0
         self.accuracy_method=accuracy_method or batch_accuracy(round_prediction)
         self.callbacks.on_train_begin(**self._state())
         for epoch in range(1,nb_epochs+1):
+            self._reset_state(epoch=epoch)
             self._run_epoch(
                 epoch=epoch,
                 loader=train_loader,
-                train_mode=True)
+                mode='train')
             # validation
             if valid_loader:
                 with torch.no_grad():
@@ -102,9 +109,11 @@ class Trainer(object):
                     self._run_epoch(
                         epoch=epoch,
                         loader=valid_loader,
-                        train_mode=False)
+                        mode='valid')
                     self.callbacks.on_validation_end(**self._state())
-            self.callbacks.on_epoch_end(**self._state())
+                self._check_for_best(epoch=epoch,loss=self.val_loss)
+            else:
+                self._check_for_best(epoch=epoch,loss=self.loss)
         self.callbacks.on_train_end(**self._state())
 
 
@@ -123,26 +132,38 @@ class Trainer(object):
     def _update_state(self,**kwargs):
         for attr in BASE_STATE_ATTRIBUTES:
             val = kwargs.get(attr,None)
-            if val:
-                setattr(self,attr,val)
-        if kwargs.get('batch_loss'):
+            if val: setattr(self,attr,val)
+        if kwargs.get('batch_loss') and kwargs.get('batch_acc'):
+            self._set_scores(**kwargs)
+
+
+    def _set_scores(self,mode=None,batch_loss=None,batch_acc=None,**kwargs):
+        if mode=='train':
+            self.batch_loss=batch_loss
+            self.batch_acc=batch_acc
             self.total_loss+=self.batch_loss
             self.loss=self.total_loss/self.batch
-        if kwargs.get('batch_acc'):
             self.acc=((self.acc*(self.batch-1))+self.batch_acc)/self.batch
+        else:
+            self.val_batch_loss=batch_loss
+            self.val_batch_acc=batch_acc
+            self.val_total_loss+=self.val_batch_loss
+            self.val_loss=self.val_total_loss/self.batch
+            self.val_acc=((self.val_acc*(self.batch-1))+self.val_batch_acc)/self.batch
 
 
     def _run_epoch(self,
             epoch,
             loader,
-            train_mode):
-        self._reset_state(epoch=epoch)
+            mode):
+        self._update_state(mode=mode)
         self.callbacks.on_epoch_begin(**self._state())
         for i,batch in enumerate(loader):
-            self._run_batch(i+1,batch,train_mode)
+            self._run_batch(i+1,batch,mode)
+        self.callbacks.on_epoch_end(**self._state())
 
 
-    def _run_batch(self,batch_index,batch,train_mode):
+    def _run_batch(self,batch_index,batch,mode):
         self._update_state(batch=batch_index)
         self.callbacks.on_batch_begin()
         inputs, targets=self._batch_data(batch)
@@ -151,10 +172,11 @@ class Trainer(object):
         self.callbacks.on_forward_end(**self._state())
         loss=self.criterion(outputs,targets)
         self._update_state(
+            mode=mode,
             batch_loss=loss.item(),
-            batch_acc=self.accuracy_method(outputs,targets))
+            batch_acc=self.accuracy_method(outputs,targets).item())
         self.callbacks.on_loss_computed(**self._state())
-        if train_mode:
+        if mode=='train':
             self.optimizer.zero_grad()
             self.callbacks.on_backward_begin(**self._state())
             loss.backward()
@@ -167,5 +189,17 @@ class Trainer(object):
         inputs=batch[INPT_KEY].float().to(self.device)
         targets=batch[TARG_KEY].float().to(self.device)
         return inputs, targets
+
+
+    def _check_for_best(self,epoch,loss):
+        if self.best_loss>loss:
+            self.best_loss=loss
+            self.best_epoch=epoch
+
+
+
+
+
+
 
 
