2902ba1cef1f71c4b8a4f819a061cf8ad603d7b9
Brookie Guzder-Williams <bguzder-williams@wri.org>
2019-04-03 15:58:33 -0700

    one_cycle: momentum

--- pytorch_nns/callbacks/one_cycle.py
+++ pytorch_nns/callbacks/one_cycle.py
@@ -21,6 +21,11 @@ class OneCycle(Callback):
             out_pct=5,
             init_reduction=10,
             min_reduction=100,
+            cycle_momentum=True,
+            max_momentum=0.95,
+            min_momentum=0.85,
+            momentum_key='betas',
+            momentum_index=0,
             name=DEFAULT_NAME):
         super(OneCycle,self).__init__()
         self.optimizer=optimizer
@@ -29,12 +34,20 @@ class OneCycle(Callback):
         self.max_lr=max_lr
         self.init_lr=max_lr/init_reduction
         self.min_lr=max_lr/min_reduction
+        self.cycle_momentum=cycle_momentum
+        self.max_momentum=max_momentum
+        self.min_momentum=min_momentum
+        self.momentum_key=momentum_key
+        self.momentum_index=momentum_index
         self.name=name
 
 
     def on_batch_begin(self,**kwargs):
         lr=self._get_lr(kwargs['batch'])
         self._update_lr(lr)
+        if self.cycle_momentum:
+            mom=self._get_mom(kwargs['batch'])
+            self._update_mom(mom)
 
 
     def _get_lr(self,batch):
@@ -56,6 +69,21 @@ class OneCycle(Callback):
         return self._line(y_in,y_out,steps,step)
 
 
+    def _get_mom(self,batch):
+        if batch<self.halfcycle_len:
+            y_in=self.max_momentum
+            y_out=self.min_momentum
+            step=batch
+            return self._line(y_in,y_out,self.halfcycle_len,step)
+        elif batch<2*self.halfcycle_len:
+            y_in=self.min_momentum
+            y_out=self.max_momentum
+            step=batch-self.halfcycle_len
+            return self._line(y_in,y_out,self.halfcycle_len,step)
+        else:
+            return self.max_momentum
+
+
     def _line(self,y_in,y_out,steps,step):
         return y_in+(step-1)*(y_out-y_in)/steps
 
@@ -63,4 +91,12 @@ class OneCycle(Callback):
     def _update_lr(self,lr):
         for grp in self.optimizer.param_groups:
             grp['lr']=lr
+
+
+    def _update_mom(self,mom):
+        for grp in self.optimizer.param_groups:
+            if isinstance(self.momentum_index,int):
+                grp[self.momentum_key][self.momentum_index]=mom
+            else:
+                grp[self.momentum_key]=mom
 
