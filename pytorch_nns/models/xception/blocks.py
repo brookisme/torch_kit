c6bd4f1cdf418d31b23b01d822ea2f8896178c70
Brookie Guzder-Williams <bguzder-williams@wri.org>
2019-06-05 14:38:11 -0700

    xception: stub out model/blocks - 2

--- pytorch_nns/models/xception/blocks.py
+++ pytorch_nns/models/xception/blocks.py
@@ -3,43 +3,133 @@ import torch.nn as nn
 
 
 class SeparableConv2d(nn.Module):
-    def __init__(self,
+    def __init__(
+            self,
             in_ch,
             out_ch=None,
             kernel_size=3,
             stride=1,
             bias=False,
             batch_norm=True,
-            act='relu',
-            act_kwargs={}):
+            act='ReLU',
+            act_config={}):
         super(SeparableConv2d, self).__init__()
         pass
 
 
+
+class EntryBlock(nn.Module):
+    """ Xception Entry Block """
+    def __init__(
+            self,
+            in_ch,
+            entry_ch=32,
+            entry_out_ch=64
+        ):
+        super(EntryBlock, self).__init__()
+        self.sconv=SeparableConv2d(
+            in_ch=in_ch,
+            out_ch=entry_ch,
+            stride=2)
+        self.conv=Conv(
+            in_ch=entry_ch
+            out_ch=entry_out_ch)
+
+
+    def forward(self,x):
+        x=self.sconv(x)
+        return self.conv(x)
+
+
+
 class  XBlock(object):
-    """ entry: 128 256 728 """
+    """ Xception Block """
     def __init__(self,
             in_ch,
             out_ch=None,
             depth=3
         ):
         super(XBlock, self).__init__()
-        pass
-        
+        if out_ch is None:
+            out_ch=in_ch
+        self.sconv_in=SeparableConv2d(in_ch,out_ch)
+        self.sconv_blocks_depth=depth-2
+        if sconv_blocks_depth:
+            self.sconv_blocks=self._sconv_blocks(out_ch)
+        self.sconv_strided=SeparableConv2d(
+                in_ch=out_ch,
+                out_ch=out_ch,
+                stride=2)
+        self.pointwise_conv=Conv(
+            in_ch=in_ch
+            out_ch=out_ch,
+            stride=2,
+            kernel_size=1)
+
+
+    def forward(self,x):
+        xpc=self.pointwise_conv(x)
+        x=self.sconv_in(x)
+        if sconv_blocks_depth:
+            x=self.sconv_blocks(x)
+        x=self.sconv_strided(x)
+        return x.add_(xpc)
+
+
+    def _sconv_blocks(self,ch):
+        blocks=[]
+        for _ in range(self.sconv_blocks_depth):
+            blocks.append(SeparableConv2d(ch))
+        return nn.ModuleList(blocks)
+
 
 class SeparableResStack(object):
-    """  bottleneck """
+    """  Xception Middle Flow """
     def __init__(self,
             ch,
             depth=16,
         ):
         super(SeparableResStack, self).__init__()
-        pass        
+        self.sconvs=self._sconv_blocks(ch,depth)
+
+
+    def forward(self,x):
+        return x.add_(sconvs(x))
+
+
+    def _sconv_blocks(self,ch,depth):
+        blocks=[]
+        for _ in range(depth):
+            blocks.append(SeparableConv2d(ch))
+        return nn.ModuleList(blocks)
 
 
 
 class SeparableStack(object)
-    """ exit """
-    def __init__(self,in_ch,*out_chs):
-        super(ConvStack, self).__init__()
-        pass  
+    """ TODO: 
+    * CHECK THIS
+    * USE FOR EXITSTACK/SeparableResStack/XBLOCK 
+    """
+    def __init__(self,in_ch,out_ch=None,depth=1,out_chs=None):
+        super(SeparableStack, self).__init__()
+        if out_ch:
+            out_chs=[out_ch]*depth
+        self.sconvs=self._sconv_blocks(in_ch,out_chs)
+
+
+    def forward(self,x):
+        return x.add_(sconvs(x))
+
+
+    def _sconv_blocks(self,ch,depth):
+        blocks=[]
+        for ch in range(out_chs):
+            blocks.append(SeparableConv2d(in_ch,ch))
+            in_ch=ch
+        return nn.ModuleList(blocks)
+
+
+
+
+
+
--- pytorch_nns/models/xception/model.py
+++ pytorch_nns/models/xception/model.py
@@ -11,35 +11,62 @@ class Xception(nn.Module):
     Xception Backbone from DeeplabV3+
     
     Args:
-        entry_ch=32,
-        entry_out_ch=64,
-        xblock_chs=[128,256,728],
-        bottleneck_depth=16,
-        exit_xblock_ch=1024,
-        exit_stack_chs=[1536,1536,2048]
+        in_ch
+        entry_ch: 32
+        entry_out_ch: 64
+        xblock_chs: [128,256,728]
+        bottleneck_depth: 16
+        exit_xblock_ch: 1024
+        exit_stack_chs: [1536,1536,2048]
+        xblock_depth: 3
+        
     Properties:
     Links:
 
     """
     def __init__(self,
+            in_ch,
             entry_ch=32,
             entry_out_ch=64,
             xblock_chs=[128,256,728],
             bottleneck_depth=16,
             exit_xblock_ch=1024,
-            exit_stack_chs=[1536,1536,2048]):
+            exit_stack_chs=[1536,1536,2048],
+            xblock_depth=3):
         super(Xception,self).__init__()
-        self.entry_block
-        self.xblocks
-        self.bottleneck
-        self.exit_xblock
-        self.exit_stack
+        self.entry_block=blocks.EntryBlock(in_ch,entry_ch,entry_out_ch)
+        self.xblocks=self._xblocks(entry_out_ch,xblock_chs,xblock_depth)
+        self.bottleneck=blocks.SeparableResStack(
+            ch=xblock_chs[-1],
+            depth=bottleneck_depth )
+        self.exit_xblock=blocks.XBlock(
+                in_ch=xblock_chs[-1],
+                out_ch=exit_xblock_ch,
+                dept=xblock_depth )
+        self.exit_stack=blocks.SeparableStack(*exit_stack_chs)
 
 
     def forward(self,x):
-        x=self.entry_block
-        x=self.xblocks
-        x=self.bottleneck
-        x=self.exit_xblock
-        x=self.exit_stack
+        x=self.entry_block(x)
+        x=self.xblocks(x)
+        x=self.bottleneck(x)
+        x=self.exit_xblock(x)
+        x=self.exit_stack(x)
         return x
+
+
+    #
+    # INTERNAL
+    #
+    def _xblocks(self,in_ch,xblock_chs,xblock_depth):
+        blocks=[]
+        for ch in xblock_chs:
+            blocks.append(blocks.XBlock(in_ch,ch,xblock_depth))
+            in_ch=ch
+        return nn.ModuleList(blocks)
+
+
+
+
+
+
