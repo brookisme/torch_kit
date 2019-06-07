bcd1d93593397bca640882120c5d173ccfe99339
Brookie Guzder-Williams <bguzder-williams@wri.org>
2019-06-07 10:43:57 -0700

    add deeplab.blocks.ASPP: first pass - need to fix channel issues

---
+++ pytorch_nns/models/deeplab/blocks.py
@@ -0,0 +1,144 @@
+import torch
+import torch.nn as nn
+
+#
+# CONSTANTS
+#
+DEFAULT_DROPOUT=0.5
+CROP_TODO="TODO: Need to crop 1x1 Conv to implement non-same padding"
+
+
+#
+# GENERAL BLOCKS
+#
+class ASPP(nn.Module):
+    """ Atrous Spatial Pyramid Pooling
+
+    Args:
+        in_ch<int>: number of input channels
+        out_ch<int|None>: if out_ch is None out_ch=in_ch
+        kernel_sizes<list[int]>: kernel_size for each conv layer
+        dilations<list[int]>: dilation for each conv layer
+        pooling<bool>: include image_pooling block
+
+    """ 
+    #
+    # CONSTANTS
+    #
+    SAME='same'
+    AVERAGE='avg'
+    MAX='max'
+    RELU='ReLU'
+
+    #
+    # STATIC METHODS
+    #
+    @staticmethod
+    def same_padding(kernel_size,dilation=1):
+        size=kernel_size+((kernel_size-1)*(dilation-1))
+        return int((size-1)//2)
+
+
+    #
+    # INSTANCE METHODS
+    #
+    def __init__(
+            self,
+            in_ch,
+            out_ch=None,
+            kernel_sizes=[1,3,3,3],
+            dilations=[1,6,12,18],
+            pooling=AVERAGE,
+            batch_norm=True,
+            bias=None,
+            out_kernel_size=1,
+            out_batch_norm=True,
+            act=RELU,
+            act_config={},
+            dropout=False):
+        super(ASPP, self).__init__()
+        if out_ch is None:
+            out_ch=in_ch
+        self.in_ch=in_ch
+        self.out_ch=out_ch
+        if not bias:
+            bias=(not batch_norm)
+        self.batch_norm=batch_norm
+        self.bias=bias
+        self.pooling=self._pooling(pooling)
+        self.batch_norms=self._batch_norms(batch_norm,in_ch,len(kernel_sizes))
+        self.aconv_list=self._aconv_list(kernel_sizes,dilations)
+        self.out_conv=self._out_conv(in_ch,out_ch,out_kernel_size,out_batch_norm)
+        self.act=self._act_layer(act,act_config)
+
+
+    def forward(self, x):
+        stack=[l(x) for l in self.aconv_list]
+        if self.pooling:
+            stack.append(self.pooling(x))
+        x=torch.cat(stack) 
+
+        if self.act:
+            x=self.act(x)
+        return x
+
+
+    def _aconv_list(self,kernels,dilations):
+        aconvs=[self._aconv(k,d,i) for i,(k,d) in  enumerate(zip(kernels,dilations))]
+        return nn.ModuleList(aconvs)
+
+
+    def _aconv(self,kernel,dilation,index):
+        aconv=nn.Conv2d(
+            in_channels=self.in_ch,
+            out_channels=self.out_ch,
+            kernel_size=kernel,
+            dilation=dilation,
+            padding=ASPP.same_padding(kernel,dilation),
+            bias=self.bias)
+        layers=[aconv]
+        if self.batch_norms:
+            layers.append(self.batch_norms[index])
+        return nn.Sequential(*layers)
+
+
+    def _pooling(self,pooling):
+        if pooling:
+            if pooling==ASPP.AVERAGE:
+                pooling=nn.AdaptiveAvgPool2d(None)
+            elif pooling==MAX:
+                pooling=nn.AdaptiveMaxPool2d(None)
+            else:
+                raise NotImplementedError("pooling must be 'avg' or 'max'")
+        else:
+            pooling=False
+        return pooling
+
+
+    def _batch_norms(self,batch_norm,in_ch,count):
+        if batch_norm:
+            batch_norms=nn.ModuleList([ nn.BatchNorm2d(in_ch) for _ in range(count) ])
+        else:
+            batch_norms=False
+        return batch_norms
+
+
+    def _out_conv(self,in_ch,out_ch,kernel_size,batch_norm):
+        conv=nn.Conv2d(
+            in_ch,
+            out_ch,
+            kernel_size=kernel_size,
+            padding=ASPP.same_padding(kernel_size))
+        layers=[conv]
+        if batch_norm:
+            layers.append(nn.BatchNorm2d(in_ch))
+        return nn.Sequential(*layers)
+
+
+
+    def _act_layer(self,act,act_config):
+        if act:
+            if isinstance(act,str):
+                act=getattr(nn,act)
+            return act(**act_config)
+
