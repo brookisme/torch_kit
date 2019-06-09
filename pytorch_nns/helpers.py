0a3d30469c1278de969eb8fe5efa3c3d352ee521
Brookie Guzder-Williams <bguzder-williams@wri.org>
2019-06-08 22:14:29 -0700

    helpers: remove output_act.  remove pytorch_nns.layers

--- pytorch_nns/helpers.py
+++ pytorch_nns/helpers.py
@@ -122,31 +122,6 @@ def get_model(
     return model
 
 
-def get_output_activation(output_activation=None,out_ch=None,**act_kwargs):
-    if isinstance(output_activation,str):
-        if output_activation.lower()=='sigmoid':
-            act=nn.Sigmoid()
-        elif output_activation.lower()=='softmax':
-            act=nn.Softmax(dim=1)
-        elif output_activation.lower()=='log_softmax':
-            act=nn.LogSoftmax(dim=1)
-        else:
-            raise ValueError(ACT_ERROR_TMPL.format(output_activation))
-    elif output_activation is None:
-        if out_ch==1:
-            act=nn.Sigmoid()
-        else:
-            act=nn.Softmax(dim=1)
-    elif output_activation is False:
-        act=False
-    else:
-        if callable(output_activation()):
-            act=output_activation(**act_kwargs)
-        else:
-            act
-    return act 
-
-
 def freeze_layer(layer):
     for param in layer.parameters():
         param.requires_grad=False
